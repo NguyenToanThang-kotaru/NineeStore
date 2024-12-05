312438e914463c49354c8b1c235505e3ba396bab
NguyenToanThang-kotaru <NguyenToanThang15122005@gmail.com>
2024-12-06 01:23:50 +0700

    Da canh le

--- admin.js
+++ admin.js
@@ -377,56 +377,48 @@ document.addEventListener("DOMContentLoaded", function () {
             });
             bodyContent +=
                 `<tr>
-                                      <td>${productName}</td>
-                                      <td><span class="product-tke-quantity-value">${product.Quantity
-                }</span></td>
-                                      <td><span class="product-tke-price-value">${(
-                    productPrice * product.Quantity
-                ).toLocaleString(
-                    "de-DE"
-                )}</span><sup>₫</sup></td>
-                                      <td>
-                                          <button class="show-hoadon-kh" onclick="showPaymentProduct(this)">Xem</button>
-                                          <div class='overlay'>
-                                              <div class="hoa-don-container" id="hoadon-items">
-                                                  <i class="fa-solid fa-rectangle-xmark close"></i>
-                                                  <div class="hoa-don-header">
-                                                      <h1>Hóa đơn</h1>
-                                                  </div>
-                                                  <div class="hoa-don-info">
-                                                      <div class="hoa-don-mat-hang">
-                                                          <h3>Thông tin sản phẩm</h3>
-                                                          <p>Mã: ${product.ID
-                }5</p>
-                                                          <p>Tên: ${productName}</p>
-                                                          <p>Nơi Bán: Nine Store</p>
-                                                      </div>
-                                                  </div>
-                                                  <div class="product-table-hd">
-                                                      <table class="product-table-info-hd">
-                                                          <thead>
-                                                              <tr class="table-header-hd">
-                                                                  <th>Tên khách hàng</th>
-                                                                  <th>Số lượng</th>
-                                                                  <th>Đã chi</th>
-                                                              </tr>
-                                                          </thead>
-                                                          <tbody>` +
-                hdContent +
-                `</tbody>
-                                                      </table>
-                                                  </div>
-                                                  <div class="total-hd">
-                                                      <hr />
-                                                      <div class="total-container-hd">
-                                                          <p class="text-hd">Tổng tiền:</p>
-                                                          <p class="total-amount-hd">120.000.000</p>
-                                                      </div>
-                                                  </div>
-                                              </div>
-                                          </div>
-                                      </td>
-                                  </tr>`;
+                    <td>${productName}</td>
+                    <td><span class="product-tke-quantity-value">${product.Quantity}</span></td>
+                    <td><span class="product-tke-price-value">${(productPrice * product.Quantity).toLocaleString("de-DE")}</span><sup>₫</sup></td>
+                    <td>
+                        <button class="show-hoadon-kh" onclick="showPaymentProduct(this)">Xem</button>
+                        <div class='overlay'>
+                            <div class="hoa-don-container" id="hoadon-items">
+                                <i class="fa-solid fa-rectangle-xmark close"></i>
+                                <div class="hoa-don-header">
+                                    <h1>Hóa đơn</h1>
+                                </div>
+                                <div class="hoa-don-info">
+                                    <div class="hoa-don-mat-hang">
+                                        <h3>Thông tin sản phẩm</h3>
+                                        <p>Mã: ${product.ID}5</p>
+                                        <p>Tên: ${productName}</p>
+                                        <p>Nơi Bán: Nine Store</p>
+                                    </div>
+                                </div>
+                                <div class="product-table-hd">
+                                    <table class="product-table-info-hd">
+                                        <thead>
+                                            <tr class="table-header-hd">
+                                                <th>Tên khách hàng</th>
+                                                <th>Số lượng</th>
+                                                <th>Đã chi</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>` + hdContent + `</tbody>
+                                    </table>
+                                </div>
+                                <div class="total-hd">
+                                        <hr />
+                                        <div class="total-container-hd">
+                                            <p class="text-hd">Tổng tiền:</p>
+                                            <p class="total-amount-hd">120.000.000</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>`;
             totalRevenue += productPrice * product.Quantity;
         });
         bodyTable.innerHTML = bodyContent;
